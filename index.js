7de629b58175c8a88af7091f486a40426051396e
burbridw <111381782+burbridw@users.noreply.github.com>
2022-08-27 03:28:33 +0900

    ace and card visuals
    Update to improve Ace functionality and to add visual representations of the cards , for all 4 suits.

--- index.js
+++ index.js
@@ -20,18 +20,88 @@ const betEl = document.getElementById("bet")
 const dealerEl = document.getElementById("dealer-cards")
 const dealerSumEl = document.getElementById("dealer-sum")
 const dealerPlaying = document.getElementById("dealer-playing")
+const cardAs = document.getElementById("cardAs")
+const card2s = document.getElementById("card2s")
+const card3s = document.getElementById("card3s")
+const card4s = document.getElementById("card4s")
+const card5s = document.getElementById("card5s")
+const card6s = document.getElementById("card6s")
+const card7s = document.getElementById("card7s")
+const card8s = document.getElementById("card8s")
+const card9s = document.getElementById("card9s")
+const card10s = document.getElementById("card10s")
+const cardJs = document.getElementById("cardJs")
+const cardQs = document.getElementById("cardQs")
+const cardKs = document.getElementById("cardKs")
+
+const cardAh = document.getElementById("cardAh")
+const card2h = document.getElementById("card2h")
+const card3h = document.getElementById("card3h")
+const card4h = document.getElementById("card4h")
+const card5h = document.getElementById("card5h")
+const card6h = document.getElementById("card6h")
+const card7h = document.getElementById("card7h")
+const card8h = document.getElementById("card8h")
+const card9h = document.getElementById("card9h")
+const card10h = document.getElementById("card10h")
+const cardJh = document.getElementById("cardJh")
+const cardQh = document.getElementById("cardQh")
+const cardKh = document.getElementById("cardKh")
+
+const cardAc = document.getElementById("cardAc")
+const card2c = document.getElementById("card2c")
+const card3c = document.getElementById("card3c")
+const card4c = document.getElementById("card4c")
+const card5c = document.getElementById("card5c")
+const card6c = document.getElementById("card6c")
+const card7c = document.getElementById("card7c")
+const card8c = document.getElementById("card8c")
+const card9c = document.getElementById("card9c")
+const card10c = document.getElementById("card10c")
+const cardJc = document.getElementById("cardJc")
+const cardQc = document.getElementById("cardQc")
+const cardKc = document.getElementById("cardKc")
+
+const cardAd = document.getElementById("cardAd")
+const card2d = document.getElementById("card2d")
+const card3d = document.getElementById("card3d")
+const card4d = document.getElementById("card4d")
+const card5d = document.getElementById("card5d")
+const card6d = document.getElementById("card6d")
+const card7d = document.getElementById("card7d")
+const card8d = document.getElementById("card8d")
+const card9d = document.getElementById("card9d")
+const card10d = document.getElementById("card10d")
+const cardJd = document.getElementById("cardJd")
+const cardQd = document.getElementById("cardQd")
+const cardKd = document.getElementById("cardKd")
+
+const cardValue = {
+    1: 11,
+    2: 2,
+    3: 3,
+    4: 4,
+    5: 5,
+    6: 6,
+    7: 7,
+    8: 8,
+    9: 9,
+    10: 10,
+    11: 10,
+    12: 10,
+    13: 10,
+    14: 1
+}
+const cardImgArrS = [cardAs,card2s,card3s,card4s,card5s,card6s,card7s,card8s,card9s,card10s,cardJs,cardQs,cardKs,cardAs]
+const cardImgArrH = [cardAh,card2h,card3h,card4h,card5h,card6h,card7h,card8h,card9h,card10h,cardJh,cardQh,cardKh,cardAh]
+const cardImgArrC = [cardAc,card2c,card3c,card4c,card5c,card6c,card7c,card8c,card9c,card10c,cardJc,cardQc,cardKc,cardAc]
+const cardImgArrD = [cardAd,card2d,card3d,card4d,card5d,card6d,card7d,card8d,card9d,card10d,cardJd,cardQd,cardKd,cardAd]
 
 cashEl.textContent = "$" + cash
 
 function getRandomCard() {
-    let randomNumber = Math.floor( Math.random()*13 ) + 1
-    if (randomNumber > 10) {
-        return 10
-    } else if (randomNumber === 1) {
-        return 11
-    } else {
-        return randomNumber
-    }
+    let randomNumber = Math.floor( Math.random()*13 ) + 1;
+    return randomNumber
 }
 
 function startGame() {
@@ -52,14 +122,12 @@ function startGame() {
     let firstCard = getRandomCard()
     let secondCard = getRandomCard()
     cards = [firstCard, secondCard]
-    sum = firstCard + secondCard
+    sum = cardValue[cards[0]] + cardValue[cards[1]]
     for (let i = 0; i < cards.length; i++) {
-        if (cards[i] === 11) {
+        if (cards[i] === 1) {
             playerAceCount += 1
         }
-    }
-    
-    renderGame()
+    } renderGame()
     } else {
         message = "You can't make that bet"
         messageEl.textContent = message
@@ -69,7 +137,16 @@ function startGame() {
 function renderGame() {
     cardsEl.textContent = "Cards: "
     for (let i = 0; i < cards.length; i++) {
-        cardsEl.textContent += cards[i] + " "
+       /* cardsEl.textContent += cards[i] + " "*/
+       if (Math.floor( Math.random()*4 ) === 0) {
+        cardsEl.appendChild(cardImgArrS[cards[i]-1].cloneNode(true))
+       } else if (Math.floor( Math.random()*4 ) === 1) {
+        cardsEl.appendChild(cardImgArrH[cards[i]-1].cloneNode(true))
+       } else if (Math.floor( Math.random()*4 ) === 2) {
+        cardsEl.appendChild(cardImgArrC[cards[i]-1].cloneNode(true))
+       } else {
+        cardsEl.appendChild(cardImgArrD[cards[i]-1].cloneNode(true))
+       }
     }
     sumEl.textContent = "Sum: " + sum
     if (cards.length === 5 && sum <= 21) {
@@ -105,12 +182,12 @@ function renderGame() {
 function newCard() {
     if (isAlive === true && hasBlackJack === false && playerHolds === false) {
         let card = getRandomCard()
-        if (card === 11 && sum > 10) {
-            card = 1
-        } else if (card === 11 && sum <= 10) {
+        if (card === 1 && sum > 10) {
+            card = 14
+        } else if (card === 1 && sum <= 10) {
             playerAceCount += 1
         }
-        sum += card
+        sum += cardValue[card]
         cards.push(card)
         renderGame()        
     }
@@ -125,9 +202,9 @@ function hold() {
     let dealerFirstCard = getRandomCard()
     let dealerSecondCard = getRandomCard()
     dealerCards = [dealerFirstCard, dealerSecondCard]
-    dealerSum = dealerFirstCard + dealerSecondCard
+    dealerSum = cardValue[dealerCards[0]] + cardValue[dealerCards[1]]
     for (let i = 0; i < dealerCards.length; i++) {
-        if (dealerCards[i] === 11) {
+        if (dealerCards[i] === 1) {
             dealerAceCount += 1
             
         }
@@ -140,7 +217,12 @@ function renderDealer() {
     dealerPlaying.textContent = "Dealer Playing"
     dealerEl.textContent = "Dealer's Cards: "
     for (let i = 0; i < dealerCards.length; i++) {
-        dealerEl.textContent += dealerCards[i] + " "
+       /* dealerEl.textContent += dealerCards[i] + " "*/
+       if (Math.floor( Math.random()*2) !== 1) {
+        dealerEl.appendChild(cardImgArrS[dealerCards[i]-1].cloneNode(true))
+       } else {
+        dealerEl.appendChild(cardImgArrH[dealerCards[i]-1].cloneNode(true))
+       }
     } 
     dealerSumEl.textContent = "Sum: " + dealerSum
     if (dealerCards.length === 5 && dealerSum <= 21) {
@@ -157,11 +239,14 @@ function renderDealer() {
         if (sum > dealerSum){
             message = "You're a winner"
             messageEl.textContent = message
+            dealerPlaying.textContent = "Dealer loses"
             cash = cash + (currentBet*2)
             cashEl.textContent = "$" + cash
+
         } else if (sum === dealerSum) {
             message = "Push, all bets refunded - Hit NEW GAME to begin"
             messageEl.textContent = message
+            dealerPlaying.textContent = "Push"
             cash = cash + (currentBet*1)
             cashEl.textContent = "$" + cash
             isAlive = false
@@ -214,10 +299,10 @@ function renderDealer() {
 function dealerNewCard() {
     if (dealerIsAlive === true && dealerHasBlackJack === false) {
         let card = getRandomCard()
-        if (card === 11 && dealerSum > 10) {
-            card = 1
+        if (card === 1 && dealerSum > 10) {
+            card = 14
         } 
-        dealerSum += card
+        dealerSum += cardValue[card]
         dealerCards.push(card)
         renderDealer()        
     }
